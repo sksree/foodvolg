ef964f285018d7c5952d4da2df8b6921a6cf719d
sreekuttan <sksree0812@gmail.com>
2022-02-11 22:48:04 +0530

    display datas in home page

--- skworkproject/productapp/views.py
+++ skworkproject/productapp/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    prod=products.objects.all()
+    ct=categ.objects.all()
+    return render(request,"home.html",{'pr':prod,'ct':ct})
